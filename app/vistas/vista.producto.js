bc248cdaa83b77e24f02c07925d0285b83d4006d
davidAI04 <david.rb.dev@gmail.com>
2021-07-22 17:03:10 -0500

    fix

--- app/vistas/vista.producto.js
+++ app/vistas/vista.producto.js
@@ -4,8 +4,8 @@ module.exports.appListar = async function (app) {
   app.get('/productos', async (req, res) => {
     let data = req.body.metodo;
     try {
-      //let resultado = await controladorDeProducto.listarProducto(data);
-      res.render("index", { result: 'resultado' });
+      let resultado = await controladorDeProducto.listarProducto(data);
+      res.render("index", { result: resultado });
     } catch (error) {
       res.status(400).json('Error en la consulta')
     }
